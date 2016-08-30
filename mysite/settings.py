b69b32fff908f83f23dd36688d4db174a917014a
TX <tangxinrt@gmail.com>
2016-08-31 01:00:43 +0800

    1

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static/"),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
