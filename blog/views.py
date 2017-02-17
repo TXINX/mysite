ed82934d27d12e62543c4f5d11ebf319ec4ffbcf
TX <tangxinrt@gmail.com>
2017-02-17 09:55:17 +0800

    none

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = reversed(Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date'))
     return render(request, 'post_list.html', {'posts': posts})
