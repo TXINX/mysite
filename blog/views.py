b0c50d38e9bd199ffa3974069cec7fade13b0c89
txnya <tangxinrt@gmail.com>
2017-02-21 14:56:28 +0800

    change name of template

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .content_parser import convert_all
 
 def post_list(request):
     articles = reversed(Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date'))
-    return render(request, 'post_list.html', {'posts': articles})
+    return render(request, 'posts.html', {'posts': articles})
 
 def refresh(request):
     convert_all(request.user)
