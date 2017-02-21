a5c491a4d0cb642cbd6186221d84ae209bd40d5b
txnya <tangxinrt@gmail.com>
2017-02-21 14:57:35 +0800

    iter mainpage

--- main_page/views.py
+++ main_page/views.py
@@ -4,10 +4,17 @@ from django.http import HttpResponse
 
 from music_player import file_path
 
+from blog.models import Article
+
 def main_page(request):
     #songs=get_object_or_404(Song)
     template = loader.get_template('main_page.html')
+    articles = list(reversed(Article.objects.order_by('published_date')))
+    #latest = Article.objects.order_by('published_date')[0]
     context = {
-        'song_audio_file_url': "musics/you.mp3"
+        'song_audio_file_url': "musics/you.mp3",
+        'latest' : articles[0],
+        'others' : articles[1:]
+
     }
     return HttpResponse(template.render(context, request))
