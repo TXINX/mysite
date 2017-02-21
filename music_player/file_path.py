1e9ef6acbc2ff7ab0bfce1f8e3e281226fd9d387
TX <tangxinrt@gmail.com>
2017-02-21 21:27:24 +0800

    try

--- music_player/file_path.py
+++ music_player/file_path.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PATH = os.path.join(BASE_DIR, r'static\musics')#r"E:\Github\mysite\test\docs"
+PATH = os.path.join(BASE_DIR, r'static/musics')#r"E:\Github\mysite\test\docs"
 
 def full_paths():
     music_names = os.listdir(PATH)
