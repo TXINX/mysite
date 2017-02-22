f93150e0b062cb9e5dfabd5a59039b1a70db699f
txnya <tangxinrt@gmail.com>
2017-02-22 15:43:51 +0800

    加入单篇文章的页面url

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
-    url(r'^$', views.post_list, name='post_list'),
+urlpatterns =\
+[
+    url(r'^blog/$', views.posts, name='blog'),
     url(r'^re/$', views.refresh, name='refresh'),
+    url(r'^$', views.main_page, name='index'),
+    url(r'^blog/(?P<index>[0-9]*)/$', views.post, name='blog'),
 ]
